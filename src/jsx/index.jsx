a65a5bbc1aab388139217423ae813e04535ab37c
Jeremy Os <50892043+Je-Real@users.noreply.github.com>
2024-02-21 13:43:30 -0600

    firsts changes
    testing and firsts moddifications to files, body and styles

---
+++ src/jsx/index.jsx
@@ -0,0 +1,20 @@
+import { useState } from "react"
+import "../css/index.css"
+
+function App() {
+	//const [count, setCount] = useState(0)
+
+	return (
+		<>
+			<h1>¡Hola! Me presento</h1>
+			<div className="card">
+				<p>
+					Edit <code>src/App.jsx</code> and save to test HMR
+				</p>
+			</div>
+			<p className="read-the-docs">Click on the Vite and React logos to learn more</p>
+		</>
+	)
+}
+
+export default App
