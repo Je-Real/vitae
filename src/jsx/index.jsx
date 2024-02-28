3ed2d20030bc61bb03b1b19518d79aaf8e07d4c4
Jeremy Os <50892043+Je-Real@users.noreply.github.com>
2024-02-28 06:10:12 -0600

    full page scroll and some functionality added
    -full page scroll added by css style
    -custom messaje depending by time in home page
    -custom fonts from google fonts imported in css style

--- src/jsx/index.jsx
+++ src/jsx/index.jsx
@@ -1,18 +1,53 @@
-import { useState } from "react"
+import { useState } from "react";
 import "../css/index.css"
 
 function App() {
-	//const [count, setCount] = useState(0)
+	const time = new Date(),
+		hour = time.getHours(),
+		salutation = () => {
+			if (hour >= 0 && hour < 12)
+				return 'Buenos dias.'
+			else if (hour >= 12 && hour < 20)
+				return 'Buenas tardes'
+			else
+				return 'Buenas noches'
+		},
+		speed = 50,
+		txt = 'Hola' + salutation,
+		typeWriter = () => {
+			if (i < txt.length) {
+				document.getElementById('title').innerHTML += txt.charAt(i);
+				i++;
+				setTimeout(typeWriter, speed);
+			}
+		}
+	
+	window.onload = function() {
+		setTimeout(typeWriter, 300);
+	};
 
 	return (
 		<>
-			<h1>¡Hola! Me presento</h1>
-			<div className="card">
-				<p>
-					Edit <code>src/App.jsx</code> and save to test HMR
-				</p>
+			<div className="scroll-container">
+				<section>
+					<div>
+						<h1 id="title" className="w-100 roboto-mono-400"></h1>
+						<h2 className="w-100">Soy yo!</h2>
+					</div>
+				</section>
+				
+				<section>
+					<h1>Section 2</h1>
+				</section>
+				
+				<section>
+					<h1>Section 3</h1>
+				</section>
+				
+				<section>
+					<h1>Section 4</h1>
+				</section>
 			</div>
-			<p className="read-the-docs">Click on the Vite and React logos to learn more</p>
 		</>
 	)
 }
